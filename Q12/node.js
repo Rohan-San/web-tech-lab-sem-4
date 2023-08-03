38a84f86030af212c809bb9fe622af21baf19de6
Rohan Santhosh <107957865+Rohan-San@users.noreply.github.com>
2023-08-03 20:44:47 +0530

    Create node.js

---
+++ Q12/node.js
@@ -0,0 +1,47 @@
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "yourusername",
+    password: "yourpassword"
+});
+
+con.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "yourusername",
+    password: "yourpassword"
+});
+
+con.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected!");
+    con.query("CREATE DATABASE mydb", function (err, result) {
+        if (err) throw err;
+        console.log("Database created");
+    });
+});
+
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+    host: "localhost",
+    user: "yourusername",
+    password: "yourpassword",
+    database: "mydb"
+});
+
+con.connect(function (err) {
+    if (err) throw err;
+    console.log("Connected!");
+    var sql = "CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))";
+    con.query(sql, function (err, result) {
+        if (err) throw err;
+        console.log("Table created");
+    });
+});
